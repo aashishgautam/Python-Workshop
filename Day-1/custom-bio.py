91a5e17cfd9539cff882169b8566fd4d44daa382
aashishgautam <gautamaashish41@gmail.com>
2025-03-16 15:15:19 +0545

    Assignment for Day 1

---
+++ Day-1/custom-bio.py
@@ -0,0 +1,12 @@
+# ''' 
+# Creating a Bio Generator
+# We'll be creating a script that does the following:
+# • Takes user name and age and other details
+# • Prints a formatted bio message
+# '''
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+age = int(age)
+age = age + 1
+print(f"Hi, I'm {name} and m {age} years old.")
--- Day-1/task1.py
+++
@@ -1,7 +0,0 @@
-''' 
-Creating a Bio Generator
-We'll be creating a script that does the following:
-• Takes user name and age and other details
-• Prints a formatted bio message
-'''
-
